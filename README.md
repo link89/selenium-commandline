fb2e27798370ecef36cb1812d93134970bfe1ee8
weihong.xu <xuweihong.cn@gmail.com>
2021-07-13 17:08:36 +0800

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # selenium-commandline
 Wrapper to install and start Selenium via npm
+
+## Introduction
+This tool is aim to simplify the installation and provision of [selenium-server](https://www.selenium.dev/downloads/).
+
+## Installation
+`npm install -g selenium-commandline`
+
+## Usage
+`selenium-cli -h`
--- commands.ts
+++ commands.ts
@@ -11,27 +11,37 @@ interface InstallOptions {
   force: boolean;
 }
 
-export async function install(downloadUrl: string = DEFAULT_DOWNLOAD_URL, opts: InstallOptions) {
-  if (fs.existsSync(SELENIUM_SERVER_PATH) && !opts.force) {
-    console.log(`selenium-server has been installed, skipped.`)
-    return;
-  }
-  console.log(`start to download: ${downloadUrl}`);
-  await download(downloadUrl, SELENIUM_SERVER_PATH);
-}
-
 interface StartOptions {
   name: string;
 }
 
+function isInstall() {
+  return fs.existsSync(SELENIUM_SERVER_PATH);
+}
+
+export async function install(downloadUrl: string = DEFAULT_DOWNLOAD_URL, opts: InstallOptions) {
+  if (isInstall() && !opts.force) {
+    console.log(`selenium-server has been installed, skipped.`)
+    return;
+  }
+  console.log(`start to download: ${downloadUrl} to ${SELENIUM_SERVER_PATH}`);
+  await download(downloadUrl, SELENIUM_SERVER_PATH);
+}
+
 export async function start(opts: StartOptions) {
+  if (!isInstall()) {
+    console.error(`Install selenium server with "selenium-cli install" first!`)
+    process.exitCode = 1;
+    return;
+  }
+
   await new Promise((resolve, reject) => {
     pm2.connect((err) => {
       if (err) {
         console.error(err);
+        process.exitCode = 1;
         return reject(err);
       }
-
       pm2.start({
         script: `java -jar ${SELENIUM_SERVER_PATH} standalone`,
         name: opts.name,
@@ -39,6 +49,7 @@ export async function start(opts: StartOptions) {
         if (err) {
           console.error(err);
           pm2.disconnect();
+          process.exitCode = 1;
           return reject(err);
         }
         console.log(`success to start ${opts.name} in pm2, check detail with "pm2 show ${opts.name}"`);
